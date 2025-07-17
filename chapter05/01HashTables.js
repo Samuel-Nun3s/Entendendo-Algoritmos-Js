ac873b3ba87a35e41b205822a63ef1aeb381447d
Samuel Nunes <samuelsalomao883@gmail.com>
2025-07-17 13:28:28 -0300

    Tabelas Hash

--- chapter05/01HashTables.js
+++ chapter05/01HashTables.js
@@ -1,10 +1,10 @@
 let voted = {};
 
 function check_voter(name) {
-  if (voted.name == name) {
+  if (voted[name]) {
     console.log("Mande embora!");
   } else {
-    voted.name = name;
+    voted[name] = true;
     console.log("Deixe votar!");
   }
 }
@@ -12,3 +12,4 @@ function check_voter(name) {
 console.log(check_voter("Samuel"));
 console.log(check_voter("Salomao"));
 console.log(check_voter("Salomao"));
+console.log(check_voter("Samuel"));
